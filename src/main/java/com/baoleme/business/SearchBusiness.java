7665900953e6abfab502ee1118cd7e6b6b872428
shenmaxg <1332840933@qq.com>
2021-10-24 18:45:36 +0800

    分词代码

--- src/main/java/com/baoleme/business/SearchBusiness.java
+++ src/main/java/com/baoleme/business/SearchBusiness.java
@@ -30,32 +30,6 @@ public class SearchBusiness {
 
     public List<Merchant> search(String content) {
         try {
-            // 分词
-//            Analyzer analyzer = new IKAnalyzer(true);
-//
-//            // 获取Lucene的TokenStream对象
-//            TokenStream ts = analyzer.tokenStream("myfield",new StringReader(content));
-//            // 获取词元位置属性
-//            OffsetAttribute offset = ts.addAttribute(OffsetAttribute.class);
-//            // 获取词元文本属性
-//            CharTermAttribute term = ts.addAttribute(CharTermAttribute.class);
-//            // 获取词元文本属性
-//            TypeAttribute type = ts.addAttribute(TypeAttribute.class);
-//
-//            // 重置TokenStream（重置StringReader）
-//            ts.reset();
-//            // 迭代获取分词结果
-//            while (ts.incrementToken()) {
-//                System.out.println(offset.startOffset() + " - " + offset.endOffset() + " : " + term.toString() + " | " + type.type());
-//            }
-//            // 关闭TokenStream（关闭StringReader）
-//            ts.end(); // Perform end-of-stream operations, e.g. set the final offset.
-//
-//            // 释放TokenStream的所有资源
-//            ts.close();
-//            analyzer.close();
-
-
             //首先去图数据库检索类别
             List<MerchantGraph> merchantGraphs = categoryBusiness.getMerchantByCategory(content);
 
@@ -75,4 +49,37 @@ public class SearchBusiness {
         }
 
     }
+
+    private List<String> getWord(String content) throws Exception {
+        List<String> words = new ArrayList<>();
+        // 分词
+        Analyzer analyzer = new IKAnalyzer(true);
+
+        // 获取Lucene的TokenStream对象
+        TokenStream ts = analyzer.tokenStream("word",new StringReader(content));
+        // 获取词元位置属性
+        OffsetAttribute offset = ts.addAttribute(OffsetAttribute.class);
+        // 获取词元文本属性
+        CharTermAttribute term = ts.addAttribute(CharTermAttribute.class);
+        // 获取词元文本属性
+        TypeAttribute type = ts.addAttribute(TypeAttribute.class);
+
+        // 重置TokenStream（重置StringReader）
+        ts.reset();
+        // 迭代获取分词结果
+        while (ts.incrementToken()) {
+            if (type.type().equals("CN_WORD")) {
+                words.add(term.toString());
+            }
+        }
+        // 关闭TokenStream（关闭StringReader）
+        ts.end(); // Perform end-of-stream operations, e.g. set the final offset.
+
+        // 释放TokenStream的所有资源
+        ts.close();
+        analyzer.close();
+
+        return words;
+    }
+
 }
