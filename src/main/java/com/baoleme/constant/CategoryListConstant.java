857dedb5b229e80be8b38a0e86825850d0982ae9
alexfarm <jy027850@126.com>
2021-10-24 18:44:36 +0800

    feature:find ads by user trace

--- src/main/java/com/baoleme/constant/CategoryListConstant.java
+++ src/main/java/com/baoleme/constant/CategoryListConstant.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class CategoryListConstant {
 
-    public final static List<String> eats = Arrays.asList("one","two","three");
+    public final static List<String> eats = Arrays.asList("火锅","披萨汉堡","烧烤烤肉","蛋糕奶茶","江浙菜","海鲜","茶餐厅");
 
-    public final static List<String> coffee = Arrays.asList("one","two","three");
+    public final static List<String> coffee = Arrays.asList("咖啡");
 
 }
